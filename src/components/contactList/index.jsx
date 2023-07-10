b4e784dcd634450e0c01edcd96956faa86960680
Rafał Chojnacki <r.chojnacki2@o2.pl>
2023-07-10 20:39:45 +0200

    Added saving in local storage

--- src/components/contactList/index.jsx
+++ src/components/contactList/index.jsx
@@ -1,60 +1,81 @@
 import React, { Component } from "react";
 import css from "./contactList.module.css";
 
-
 class ContactList extends Component {
-    state = {
-      filter:''
+  state = {
+    filter: "",
+    contacts:[]
+  };
+
+  handleChange = (e) => {
+    const { value, name } = e.target;
+    this.setState((prevState) => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  saveContactsToLocalStorage = () => {
+    const contacts = this.props.contacts;
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  };
+
+  componentDidMount() {
+    const savedContacts = localStorage.getItem("contacts");
+    if (savedContacts) {
+      const contacts = JSON.parse(savedContacts);
+      this.setState({ contacts });
     }
-    handleChange = (e) => {
-     
-      const { value, name } = e.target;
-      this.setState((prevState) => ({
-        ...prevState,
-        [name]: value,
-      }));
-    };
-  
-    getContacts = () => {
-      const filter = this.state.filter
-      const contacts = this.props.contacts
-      if (filter.length === 0) {
-        return contacts
-      }
-  
-      return contacts.filter((contact) => contact.name.toLowerCase().indexOf(filter) >= 0)
+  }
+
+  componentDidUpdate() {
+    this.saveContactsToLocalStorage();
+  }
+
+  getContacts = () => {
+    const filter = this.state.filter;
+    const contacts = this.props.contacts;
+
+    if (filter.length === 0) {
+      return contacts;
     }
-    render() {
-      return (
+
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  render() {
+    return (
+      <div>
         <div>
-          <div>
-            <h2>Contacts</h2>
-            <form className={css.form}>
-                <div className={css.formInput}> 
-                <label htmlFor="Find contacts by name" className={css.inputLabel}>
+          <h2>Contacts</h2>
+          <form className={css.form}>
+            <div className={css.formInput}>
+              <label htmlFor="Find contacts by name" className={css.inputLabel}>
                 Find contacts by name
-                <input   type="text"
+                <input
+                  type="text"
                   name="filter"
                   placeholder="finding name"
                   value={this.state.filter}
                   onChange={this.handleChange}
-                  className={css.formInput}>
-                </input>
-              </label>  
-                </div>
-             
-            </form>
-          </div>
-          <ul>
-            {this.getContacts().map(({ name, number, id }, index) => (
-              <li key={id} className={css.contact}>
-                {name} --- {number}
-              </li>
-            ))}
-          </ul>
+                  className={css.formInput}
+                />
+              </label>
+            </div>
+          </form>
         </div>
-      );
-    }
+        <ul>
+          {this.getContacts().map(({ name, number, id }, index) => (
+            <li key={id} className={css.contact}>
+              {name} --- {number}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
   }
+}
 
-  export default ContactList
+export default ContactList;
